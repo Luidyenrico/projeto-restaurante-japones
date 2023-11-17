5fd648fe08ae7e555eb879b5501d4d2750e7f77e
linsm7 <linsmatheus553@gmail.com>
2023-11-17 00:03:58 -0300

    Criação de uma banco de dados no estilo txt, com a autenticação do usuário

---
+++ src/js/cadastro.js
@@ -0,0 +1,41 @@
+function salvarCadastro() {
+    // Obtém os dados do formulário
+    var form = document.getElementById('cadastroForm');
+    var formData = new FormData(form);
+
+    // Obtém os dados existentes do localStorage ou cria um array vazio
+    var dadosCadastrados = JSON.parse(localStorage.getItem('dadosCadastrados')) || [];
+
+    // Adiciona os novos dados ao array
+    dadosCadastrados.push(Object.fromEntries(formData));
+
+    // Atualiza o localStorage com os novos dados
+    localStorage.setItem('dadosCadastrados', JSON.stringify(dadosCadastrados));
+
+    alert('Cadastro salvo com sucesso!');
+}
+
+function baixarDados() {
+    var dadosCadastrados = JSON.parse(localStorage.getItem('dadosCadastrados')) || [];
+
+    var texto = '';
+    dadosCadastrados.forEach(function(dado) {
+        for (var chave in dado) {
+            texto += chave + ': ' + dado[chave] + '\t';
+        }
+        texto += '\n';
+    });
+
+    var blob = new Blob([texto], { type: 'text/plain' });
+    var url = window.URL.createObjectURL(blob);
+
+    var link = document.createElement('a');
+    link.href = url;
+    link.download = 'dados_cadastrados.txt';
+
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+
+    window.URL.revokeObjectURL(url);
+}
---
+++ src/js/login.js
@@ -0,0 +1,22 @@
+function realizarLogin() {
+    // Obtém os dados do formulário de login
+    var form = document.getElementById('loginForm');
+    var formData = new FormData(form);
+    var emailDigitado = formData.get('email');
+    var senhaDigitada = formData.get('senha');
+
+    // Obtém os dados cadastrados do localStorage
+    var dadosCadastrados = JSON.parse(localStorage.getItem('dadosCadastrados')) || [];
+
+    // Verifica se há uma correspondência entre o email/senha digitados e os dados cadastrados
+    var usuarioValido = dadosCadastrados.some(function(dado) {
+        return dado.email === emailDigitado && dado.senha === senhaDigitada;
+    });
+
+    // Exibe uma mensagem de sucesso ou erro
+    if (usuarioValido) {
+        alert('Login bem-sucedido!');
+    } else {
+        alert('Email ou senha incorretos. Tente novamente.');
+    }
+}
